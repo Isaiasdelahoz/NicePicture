e8a1d0d27eeeb236d91d53b16864cc0fd157888b
Isaías De la Hoz <isaiasdelahoz@gmail.com>
2020-09-26 14:54:30 -0500

    initial commit

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "aSKgKsF_V8JDptBhLxhwtSr3-ibpbDoGNGYxXskQPo0"
+  config.application_secret = "ooLIi7qjbtEl7VAlXkCoc0o8YiyzEoY6kr9G6QMK-JQ"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "nicepicture"
+end
